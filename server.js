d6b05327c76d99f6399ef3036aa5fe9d14db0cd1
yeonkyu <dusrbpoiiij@naver.com>
2020-09-20 20:21:07 +0900

    add user register

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 const app = express();
+app.use(bodyParser.json());
+
 require('dotenv').config({
   path: './config/index.env'
 })
 
+// MongoDB 
+const connectDB = require('./config/db');
+connectDB();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(cors());
+
+// routes 
+app.use('/api/user/', require('./routes/auth.route'));
+
 
 app.get('/', (req, res) => {
   res.send('test route => home page');
@@ -27,3 +38,8 @@ const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 })
+
+// Let's make mongodb connection
+// Let's try to sign up user
+// express bodyParser not working let's install it 
+// Now it's work and user register successfully and generate token
